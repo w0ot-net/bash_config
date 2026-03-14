49da33d0992d7e46f1acc9f3689a9ccc3e128dcb
root <fladugi@yahoo.com>
2026-03-14 11:43:21 -0400

    Disable IPv6 router advertisement acceptance in kill6
    Prevent IPv6 addresses from reappearing after kill6 by also setting
    accept_ra=0 on all, default, and eth0 interfaces.

--- aliases/kill6.sh
+++ aliases/kill6.sh
@@ -1,1 +1,1 @@
-alias kill6='sysctl -w net.ipv6.conf.all.disable_ipv6=1;sysctl -w net.ipv6.conf.default.disable_ipv6=1'
+alias kill6='sysctl -w net.ipv6.conf.all.disable_ipv6=1;sysctl -w net.ipv6.conf.default.disable_ipv6=1;sysctl -w net.ipv6.conf.all.accept_ra=0;sysctl -w net.ipv6.conf.default.accept_ra=0;sysctl -w net.ipv6.conf.eth0.accept_ra=0'
