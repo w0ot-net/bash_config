c393a8476143649d5cff6df13874b851c6dbdb1b
root <fladugi@yahoo.com>
2026-03-14 18:26:49 -0400

    Make kill6 persistent via sysctl.d conf file
    Write IPv6 disable settings to /etc/sysctl.d/99-disable-ipv6.conf
    instead of ephemeral sysctl -w calls that get reset by dhclient.

--- aliases/kill6.sh
+++ aliases/kill6.sh
@@ -1,1 +1,4 @@
-alias kill6='sysctl -w net.ipv6.conf.all.disable_ipv6=1;sysctl -w net.ipv6.conf.default.disable_ipv6=1;sysctl -w net.ipv6.conf.all.accept_ra=0;sysctl -w net.ipv6.conf.default.accept_ra=0;sysctl -w net.ipv6.conf.eth0.accept_ra=0'
+kill6() {
+    printf "net.ipv6.conf.all.disable_ipv6=1\nnet.ipv6.conf.default.disable_ipv6=1\nnet.ipv6.conf.all.accept_ra=0\nnet.ipv6.conf.default.accept_ra=0\n" > /etc/sysctl.d/99-disable-ipv6.conf
+    sysctl --system
+}
